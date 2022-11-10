94c50104b2b66c979d9c343bb760fe2003718cff
anhasamea <anhasamea707@gmail.com>
2022-11-10 19:45:28 +0600

    data loaded at checkout page

--- src/Pages/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout.js
@@ -1,10 +1,48 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const Checkout = () => {
+    const service = useLoaderData();
+    // const{img,name,details} = service;
     return (
-        <div>
-            <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore ullam nobis aperiam harum culpa laborum debitis doloribus sint nemo, aliquam officia, quia magni deleniti est impedit quaerat ad quisquam exercitationem!</h2>
+        <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+        <div className="card w-96 bg-base-100 shadow-xl">
+        <figure className="px-10 pt-10">
+            <img src={service.facility[0].img} alt="ancient" className="rounded-xl h-64" />
+        </figure>
+        <div className="card-body ">
+            <h2 className="card-title text-start">{service.facility[0].name}</h2>
+            <p className='text-start text-slate-500'>{service.facility[0].details}</p>           
         </div>
+        </div>
+        <div className="card w-96 bg-base-100 shadow-xl">
+        <figure className="px-10 pt-10">
+            <img src={service.facility[1].img} alt="ancient" className="rounded-xl h-64" />
+        </figure>
+        <div className="card-body ">
+            <h2 className="card-title text-start">{service.facility[1].name}</h2>
+            <p className='text-start text-slate-500'>{service.facility[1].details}</p>           
+        </div>
+        </div>
+        <div className="card w-96 bg-base-100 shadow-xl">
+        <figure className="px-10 pt-10">
+            <img src={service.facility[2].img} alt="ancient" className="rounded-xl h-64" />
+        </figure>
+        <div className="card-body ">
+            <h2 className="card-title text-start">{service.facility[2].name}</h2>
+            <p className='text-start text-slate-500'>{service.facility[2].details}</p>           
+        </div>
+        </div>
+        <div className="card w-96 bg-base-100 shadow-xl">
+        <figure className="px-10 pt-10">
+            <img src={service.facility[3].img} alt="ancient" className="rounded-xl h-64" />
+        </figure>
+        <div className="card-body ">
+            <h2 className="card-title text-start">{service.facility[3].name}</h2>
+            <p className='text-start text-slate-500'>{service.facility[3].details}</p>           
+        </div>
+        </div>
+    </div>
     );
 };
 
--- src/Router/Routes/Routes.js
+++ src/Router/Routes/Routes.js
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layout/Main";
+import Checkout from "../../Pages/Checkout/Checkout";
 import About from "../../Pages/Home/About/About";
 import Home from "../../Pages/Home/Home/Home"
 import Services from "../../Pages/Home/Services/Services";
@@ -20,7 +21,13 @@ const router = createBrowserRouter([
             {
                 path:'/services',
                 element: <Services></Services>
-            }
+            },
+            {
+                path:'/services/:id',
+                element: <Checkout></Checkout>,
+                loader: ({params})=>
+                fetch(`http://localhost:5000/services/${params.id}`)
+            },
             
         ]
     }
