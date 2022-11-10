0247b718acae68f5a45d811fd75dc0b8615386c1
anhasamea <anhasamea707@gmail.com>
2022-11-10 18:25:31 +0600

    description added on service card

--- src/Pages/Home/Services/ServiceCard.js
+++ src/Pages/Home/Services/ServiceCard.js
@@ -2,15 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const ServiceCard = ({service}) => {
-    const {title,img,price,_id} = service;
+    const {title,img,price,description,_id} = service;
     return (
         <div className="card w-96 bg-base-100 shadow-xl">
         <figure className="px-10 pt-10">
             <img src={img} alt="ancient" className="rounded-xl" />
         </figure>
         <div className="card-body ">
-            <h2 className="card-title">{title}</h2>
-            
+            <h2 className="card-title text-start">{title}</h2>
+            <p className='text-start text-slate-500'>{description}</p>
             <div className="card-actions">
             <p className='text-cyan-500 font-semibold text-start pt-2'>Price: ${price}</p>
                 <Link to={`/services/${_id}`}><button className="btn-ghost text-cyan-500 font-extrabold p-2 rounded-lg">❯❯❯</button></Link>
