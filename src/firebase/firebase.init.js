9b5f0ba88faf92f22fd1adc8d12ccac404d4f2ad
anhasamea <anhasamea707@gmail.com>
2022-11-10 17:55:47 +0600

    title changed

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA7R7m0sBm-ot3LanZ9p5LX3aqmAXyDVoc",
-  authDomain: "ancient-shot.firebaseapp.com",
-  projectId: "ancient-shot",
-  storageBucket: "ancient-shot.appspot.com",
-  messagingSenderId: "225090420276",
-  appId: "1:225090420276:web:b2de78588dd408d0a2d461"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
